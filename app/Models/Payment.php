62208d38492af69329ec4ec4d4cc2c6761071848
crisanch <cristian.sanchez@triciclos.cl>
2021-09-30 12:12:35 -0300

    feat(payment): create migration, model,  factories and controllers

---
+++ app/Models/Payment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\ModelStates\HasStates;
+
+class Payment extends Model
+{
+    use HasFactory, HasStates;
+}
